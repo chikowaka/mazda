c4daf88ff95fbc1639768ed3ce8dcadac24c5815
chikowaka <45866740+chikowaka@users.noreply.github.com>
2021-07-15 13:52:09 +0900

    定期実行を実装

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,29 @@
-console.log('test');
 /* When the browser-action button is clicked... */
-chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log(tab);
-    chrome.tabs.sendMessage(tab.id, {
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     console.log(tab);
+//     chrome.tabs.sendMessage(tab.id, {
+//         command: "change_title",
+//         title: "hoge"
+//       },
+//       function(msg) {
+//         console.log("result message:", msg);
+//       });
+//   });
+console.log('test');
+chrome.alarms.create('NAME_OF_ALARM', { delayInMinutes: 1, periodInMinutes: 1 });
+chrome.alarms.onAlarm.addListener(alarm => {
+  if (alarm.name == 'NAME_OF_ALARM') {
+    console.log('on_alarm');
+    chrome.tabs.query( {title:'最新ツイート / Twitter'}, function(tabs){
+      console.log(tabs);
+      console.log(tabs[0]);
+      chrome.tabs.sendMessage(tabs[0].id, {
         command: "change_title",
-        title: "hoge"
-      },
-      function(msg) {
-        console.log("result message:", msg);
-      });
-  });
+          title: "hoge"
+        },
+        function(msg) {
+          console.log("result message:", msg);
+        });
+    });
+  }
+});
